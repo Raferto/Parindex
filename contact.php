f698ab72967ffceda2530c362fad964a92e49de2
aulkiller <58225261+aulkiller@users.noreply.github.com>
2019-11-30 22:01:41 +0700

    Merge branch 'master' of https://github.com/Raferto/Parindex

---
+++ contact.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title> Contact Us </title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+		<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+		<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+		<link href="css/style.css" rel='stylesheet' type='text/css' />
+		<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+		<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+		<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+		<link href="css/font-awesome.css" rel="stylesheet">
+		<!-- Custom Theme files -->
+		<script src="js/jquery-1.12.0.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<!--animate-->
+		<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+		<script src="js/wow.min.js"></script>
+			<script>
+				 new WOW().init();
+			</script>
+	</head>
+
+	<body>
+		<?php include('includes/header.php');?>
+		<div class="banner-1 ">
+			<div class="container">
+				<h1 class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">Siapa Kita</h1>
+			</div>
+		</div>
+		<div class="container">
+										
+
+		<h3 class="wow fadeInDown animated animated animated" style="visibility: visible; animation-name: fadeInDown; animation-delay: 0.5s;" data-wow-delay=".5s">&nbsp; &nbsp; &nbsp;Kontak kita di</h3>
+		
+		
+	<p>
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<textarea name="message" rows="5" cols="30">
+	Nama: Rafid Ferdianto
+	NRP: 05111840000032
+	No.HP: 087815014902
+	Email: rafid.ferdianto@gmail.com
+	</textarea>
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<textarea name="message" rows="5" cols="30">
+	Nama: Aulia Ihza H.
+	NRP: 05111840000089
+	No.HP: 0812812812812
+	Email: auliaihza@gmail.com
+	</textarea>
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<textarea name="message" rows="5" cols="30">
+	Nama: Kinasihurrabb M.
+	NRP: 05111840000161
+	No.HP: 081232045671
+	Email: kinas.moral@gmail.com
+	</textarea>
+											<span style='color: rgb(53, 111, 219); font-family: "Open Sans", Arial, sans-serif; font-size: 25px; text-align: justify;'>&nbsp; &nbsp; &nbsp; Rafid Ferdianto &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 05111840000032</span>
+											<br>
+											<span style='color: rgb(252,23,245); font-family: "Open Sans", Arial, sans-serif; font-size: 25px; text-align: justify;'>&nbsp; &nbsp; &nbsp; Aulia Ihza H &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 05111840000089</span>
+											<br>
+											<span style='color: rgb(70, 176, 58); font-family: "Open Sans", Arial, sans-serif; font-size: 25px; text-align: justify;'>&nbsp; &nbsp; &nbsp; Kinasihurrab M &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 05111840000161</span>
+											<br>
+
+	</p> 
+	
+
+		
+	</div>		
+		<?php include('includes/footer.php');?>
+	</body>
+
+</html>
