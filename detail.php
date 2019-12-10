a2f223146300a1a9c301e1bb825c3707dace5aeb
Raferto <rafid.ferdianto@gmail.com>
2019-12-10 23:44:15 +0700

    Update detail.php

--- detail.php
+++ detail.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Parindex-Main</title>
-
+	<title>Parindex-Detail Page</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<link href="css/style.css" rel='stylesheet' type='text/css' />
 </head>
@@ -41,8 +40,7 @@
 	<?php
 	$host = mysqli_connect("localhost","root","","parindex");
 	$id = $_GET['id'];
-	echo $id;
-	$q='SELECT NAMATEMPAT,NAMAKOTA,HTM,LOKASI,NOTELP,WEBSITE,DESKRIPSI,GROUP_CONCAT(DISTINCT tag.TAG) as TAG 
+	$data="SELECT NAMATEMPAT,NAMAKOTA,HTM,LOKASI,NOTELP,WEBSITE,DESKRIPSI,GROUP_CONCAT(DISTINCT tag.TAG) as TAG
 		FROM tempatwisata,kota,tag,termasuk
 		WHERE tempatwisata.IDTEMPAT='$id' AND
 				tag.TAG=termasuk.TAG AND
@@ -51,46 +49,38 @@
 					SELECT IDKOTA
 					FROM tempatwisata
 					WHERE IDTEMPAT='$id')
-		GROUP BY tempatwisata.IDTEMPAT';
-	$q2='SELECT FOTO
+		GROUP BY tempatwisata.IDTEMPAT";
+	$queryData_mysql = mysqli_query($host,$data);
+
+	$foto="SELECT FOTO
 		FROM fotolokasi
-		WHERE fotolokasi.IDTEMPAT='$id'';
-	$q3='SELECT fasilitas.NAMAFASILITAS,fasilitas.BIAYA
+		WHERE fotolokasi.IDTEMPAT='$id'";
+	$queryFoto_mysql = mysqli_query($host,$foto);
+
+	$fasilitas="SELECT fasilitas.NAMAFASILITAS,fasilitas.BIAYA
 		FROM fasilitas
 		WHERE fasilitas.IDFASILITAS IN(
 			SELECT IDFASILITAS
 			FROM memiliki
-			WHERE IDTEMPAT='$id')';
-	$q4='SELECT jadwal.HARI,jadwal.JAMBUKA,jadwal.JAMTUTUP
+			WHERE IDTEMPAT='$id')";
+	$queryFasilitas_mysql = mysqli_query($host,$fasilitas);
+
+	$jadwal="SELECT jadwal.HARI,jadwal.JAMBUKA,jadwal.JAMTUTUP
 			FROM jadwal
 			WHERE jadwal.IDJADWAL IN
 				(SELECT IDJADWAL
 				FROM beroprasi
 				WHERE IDTEMPAT='$id'
 				)
-			ORDER BY FIELD(Jadwal.HARI,'Senin','Selasa','Rabu','Kamis','Jumat','Sabtu','Minggu')';
-	$q5='SELECT rating.NAMAPERATING,rating.KOMENTAR,rating.SCORE
+			ORDER BY FIELD(Jadwal.HARI,'Senin','Selasa','Rabu','Kamis','Jumat','Sabtu','Minggu')";
+	$queryJadwal_mysql = mysqli_query($host,$jadwal);
+
+	$rating="SELECT rating.NAMAPERATING,rating.KOMENTAR,rating.SCORE
 			FROM rating
 			WHERE rating.IDTEMPAT=$id
-			ORDER BY rating.IDRATING
+			ORDER BY rating.IDRATING";
+	$queryRating_mysql = mysqli_query($host,$rating);
 
-			SELECT FOTO
-			FROM fotolokasi
-			WHERE fotolokasi.IDTEMPAT='$id'';
-	$q6-'INSERT INTO rating VALUES ('','$id','$namaperating','$komen',$nilai);
-		UPDATE tempatwisata SET JMHRATING = JMHRATING+1 WHERE IDTEMPAT='$id';
-		UPDATE tempatwisata SET RERATARATING = 
-			(SELECT SUM(SCORE)
-			FROM RATING
-			WHERE IDTEMPAT='$id')/JMHRATING
-		WHERE IDTEMPAT='$id';';		
-	$q7='SELECT tempatwisata.NAMATEMPAT,rating.NAMAPERATING,rating.KOMENTAR,rating.SCORE
-		FROM tempatwisata,rating
-		WHERE tempatwisata.IDTEMPAT='$id' AND rating.IDTEMPAT='$id'';
-		
 	?>
-
 </body>
-
-
 </html>
